4abe7a8c8d4d260bcaf978407f2adbf38361f4e9
Jesus Jaime Guerra Aldana <dguerra.jimmy@gmail.com>
2024-04-22 10:10:07 -0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Inicia Sesion en DevStagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{asset('img/login.jpg')}}" alt="Imagen login Usuarios">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form method="POST" action="{{route('login')}}">
+                @csrf
+                @if (session('mensaje'))
+                <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{session('mensaje')}}</p>
+                @endif
+                <div class="mb-5">
+                    <label class="mb-2 block uppercase text-gray-500 font-bold" for="email">Email</label>
+                    <input id="email" name="email" type="email" placeholder="Tu Email de Registro" class="border p-3 w-full rounded-lg @error('name')
+                    border-red-500
+                @enderror" value="{{old('email')}}">
+                    @error('email')
+                    <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{$message}}</p>
+                @enderror
+                </div>
+                <div class="mb-5">
+                    <label class="mb-2 block uppercase text-gray-500 font-bold" for="password">Password</label>
+                    <input id="password" name="password" type="password" placeholder="Password de Registro" class="border p-3 w-full rounded-lg @error('name')
+                    border-red-500
+                @enderror">
+                    @error('password')
+                    <p class="bg-red-500 text-white my-2 rounded-lg p-2 text-center">{{$message}}</p>
+                @enderror
+                </div>
+                <div class="mb-5 flex gap-2">
+                    <input type="checkbox" name="remember"> <label class="text-gray-600 italic">Mantener mi sesion abierta</label>
+                </div>
+                <input type="submit" value="Iniciar Sesion" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
